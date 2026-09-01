3f18747d01bf16ddac906861c15bf7330741d0fe
Max <mextie0@gmail.com>
2026-08-31 21:37:44 -0400

    altered testroom script for debug

--- cms-484-capstone-project/maps/multiplayer_test_room.gd
+++ cms-484-capstone-project/maps/multiplayer_test_room.gd
@@ -4,16 +4,22 @@ extends Node3D
 @onready var players := $Players
 
 func _ready():
+	print("MultiplayerTestRoom ready. is_server=", multiplayer.is_server(), " unique_id=", multiplayer.get_unique_id())
 	if multiplayer.is_server():
 		multiplayer.peer_connected.connect(_spawn_player)
 		multiplayer.peer_disconnected.connect(_despawn_player)
 		_spawn_player(multiplayer.get_unique_id())  # the host's own player
+	else:
+		multiplayer.peer_connected.connect(func(id): print("[client] peer_connected fired for: ", id))
 
 func _spawn_player(id: int):
+	print("_spawn_player called for id: ", id)
 	var player = preload("res://characters/player/player.tscn").instantiate()
 	player.name = str(id)
 	players.add_child(player)
+	print("Player node added. Players now has children: ", players.get_children())
 
 func _despawn_player(id: int):
+	print("_despawn_player called for id: ", id)
 	if players.has_node(str(id)):
 		players.get_node(str(id)).queue_free()
